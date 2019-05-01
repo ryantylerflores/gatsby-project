f29732b49a784e710516bebd1d8f151ca37eea74
Ryan Flores <rtflores90@gmail.com>
2019-05-01 10:55:17 -0700

    created new page template and created dynamic query in graphql
    created new page template and created dynamic query in graphql. Populated blog pages with data

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,52 @@
 import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // const renderPageInfo = data.allMarkdownRemark.edges.map((x,index) => {
+  //   return (
+  //     <li key={index}>
+  //       <h2>{x.node.frontmatter.title}</h2>
+  //       <p>{x.node.frontmatter.date}</p>
+  //     </li>
+  //   )
+  // })
+
   return (
     <Layout>
       <h1>Blog Page</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {/* {renderPageInfo} */}
+        { data.allMarkdownRemark.edges.map((edges,index) => {
+          return(
+            <li key={index}>
+              <Link to={`/blog/${edges.node.fields.slug}`}>
+                <h2>{edges.node.frontmatter.title}</h2>
+                <p>{edges.node.frontmatter.date}</p>
+              </Link>
+            </li>
+          )
+        }) }
+      </ol>
     </Layout>
   )
 }
